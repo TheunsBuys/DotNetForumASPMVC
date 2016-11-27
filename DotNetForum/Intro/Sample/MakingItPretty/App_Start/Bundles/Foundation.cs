d1d1b8a9f595116cb74c679c4325f569e93dbe7c
Ahmad Mahomed <ahmad@entelect.co.za>
2016-11-27 16:26:24 +0200

    Added styling components, foundation css, bundling examples, html render* examples, @sections example

---
+++ DotNetForum/Intro/Sample/MakingItPretty/App_Start/Bundles/Foundation.cs
@@ -0,0 +1,8 @@
+using System.Web.Optimization;
+
+namespace MakingItPretty
+{
+    public static class Foundation
+    {
+    }
+}
---
+++ DotNetForum/Intro/Sample/MakingItPretty/Controllers/CandidateController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using MakingItPretty.Models;
+
+namespace MakingItPretty.Controllers
+{
+    [RoutePrefix("Candidate"), Route("{action=index}/{id?}")]
+    public class CandidateController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var model = new CandidateViewModel();
+            model.Candidates = new List<CandidateViewModel>()
+            {
+                new CandidateViewModel
+                {
+                    FirstName= "Koos",
+                    LastName = "Venter",
+                    Email = "koos@yahoo.com",
+                }
+            };
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Detail(int id)
+        {
+            var model = new CandidateViewModel
+            {
+                FirstName = "Koos",
+                LastName = "Venter",
+                Email = "koos@yahoo.com",
+                LastApplication = new Application()
+                                  {
+                                      ApplicationDate = new DateTime(2016,11,11),
+                                      JobReference = "ENT001"
+        
+                                  }
+            };
+            return View( model);
+        }
+
+        [HttpGet]
+        [ChildActionOnly]
+        public PartialViewResult CandidateAdress(int id)
+        {
+            var model = new CandidateAddressViewModel
+            {
+                AddressLine1 = "13 Corlett Drive",
+                AddressLine2 = "Melrose",
+                City = "Johannesburg",
+            };
+            return PartialView("_CandidateAdress",model);
+        }
+    }
+}
