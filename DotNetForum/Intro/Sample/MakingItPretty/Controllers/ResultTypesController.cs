9bf13642cc4e293fe3842cf4eeab30d0338f3e13
Ahmad Mahomed <ahmad@entelect.co.za>
2016-11-27 17:26:40 +0200

    Added ResultTypesController and examples

---
+++ DotNetForum/Intro/Sample/MakingItPretty/Controllers/ResultTypesController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Script.Serialization;
+
+namespace MakingItPretty.Controllers
+{
+    public class ResultTypesController : Controller
+    {
+        // GET: ResultTypes
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult ViewResult()
+        {
+            return View();
+        }
+
+        public PartialViewResult PartialViewResult()
+        {
+            return PartialView();
+        }
+
+        public RedirectResult RedirectResult()
+        {
+            var url = "http://www.google.co.za";
+            return Redirect(url);
+        }
+
+        public RedirectToRouteResult RedirectToRouteResult()
+        {
+            return RedirectToAction("Index");
+            //return RedirectToActionPermanent("Index");
+
+            //var routeValues = new RouteValueDictionary();
+            //routeValues.Add("Action", "Index");
+            //routeValues.Add("Controller", "Candidate");
+
+            //return RedirectToRoute(routeValues);
+            //return RedirectToRoutePermanent(routeValues);
+        }
+
+        public ContentResult ContentResult()
+        {
+            return Content("SomeText", "text/plain");
+        }
+
+        public JsonResult JsonResult()
+        {
+            var model = new {Name = "John", Surname = "Wick"};
+            return Json(model, JsonRequestBehavior.AllowGet);
+        }
+
+        public JavaScriptResult JavaScriptResult()
+        {
+            // antipattern 
+            // use seldomly or NEVER
+
+            return JavaScript("alert('from controller')");
+        }
+
+        public FileResult FileResult()
+        {
+            return File("file.txt", "text/plain");
+        }
+
+        public EmptyResult EmptyResultAction()
+        {
+            return new EmptyResult();
+        }
+
+
+    }
+}
