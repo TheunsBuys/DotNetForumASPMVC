3f3372a5169af9da731fe235867e2c3aea11fce5
Matthew van der Velden <matthew.vdvelden@entelect.co.za>
2016-12-01 17:09:00 +0200

    Added the IntermediateWebApiSample to show async programming and DI.

---
+++ DotNetForum/Advanced/IntermediateWebApiSample/App_Start/WebApiConfig.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Http;
+using IntermediateWebApiSample.Attributes;
+
+namespace IntermediateWebApiSample
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                "DefaultApi",
+                "api/{controller}/{id}",
+                new {id = RouteParameter.Optional}
+                );
+
+            config.Filters.Add(new ValidateModelAttribute());
+                //The validation attribute can be applied to all web requests.
+        }
+    }
+}
---
+++ DotNetForum/Advanced/IntermediateWebApiSample/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace IntermediateWebApiSample.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<IntermediateWebApiSample.Persistence.Contexts.JobsContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(IntermediateWebApiSample.Persistence.Contexts.JobsContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
